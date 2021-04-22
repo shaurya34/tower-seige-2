0e8c677c931d4caa336dadae1af2341268d03e46
shaurya34 <73827874+shaurya34@users.noreply.github.com>
2021-04-22 11:05:01 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,39 @@
+class Block{
+  constructor(x, y){
+    var options={
+      restitution:0.7,
+      //density:1.2,
+      friction:0.6,
+    }
+    this.body=Bodies.rectangle(x,y,30,40,options);
+    this.x=x;
+    this.y=y;
+    this.width=30;
+    this.height=40;
+    World.add(world,this.body)
+  }
+  display(){
+   
+    if(this.body.speed<4){
+      
+      var angle = this.body.angle;
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      rectMode(CENTER);
+      rect(0,0,30,40);
+      pop();
+    }
+    else{
+      World.remove(world,this.body);
+      push();
+      this.Visibility=this.Visibility-2;
+      tint(255,this.Visibility);
+      
+      pop();
+    }
+  }
+}
+      
+      
+
